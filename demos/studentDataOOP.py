78dc47627b1612f4362f358ff0a9e451740f2e5e
aShinde <ananta.shinde@honeywellcom>
2024-07-22 10:41:46 +0530

    22/07/24

--- demos/studentDataOOP.py
+++ demos/studentDataOOP.py
@@ -1,48 +1,16 @@
+
+#entity : dictionary
+#collection : list
 class Student:
-    #properties
-
-
-    #Actions
-    def __init__(self,rollno,name,branch):
-        self.rollNo = rollno
-        self.name = name
-        self.branch = branch
-
-    def showDetails(self):
-        print("Rollno:"+str(self.rollNo) +" Name:" +self.name +" Brnach :"+self.branch)
-
-
-class StudentManager:
-
     def __init__(self):
-        self.list = []
-    #actions
-    def addStudent(self,stud):
-        self.list.append(stud)
-
-    def findStudentByRollNo(self,rollNo):
-        studRef= None
-
-        for s in self.list:
-            if(s.rollNo == rollNo ):
-               studRef = s
-
-        return studRef
-    def deleteStudent(self,rollNo):
-        studRef = self.findStudentByRollNo(rollNo)
-        self.list.remove(studRef)
-
-    def printStudentList(self):
-        for s in self.list:
-            s.showDetails()
+        self.rollNo = input("Enter roll No")
+        self.name = input("Enter name")
+        self.branch = input("enter branch")
 
 
+studentList = []
 
-s1 = Student(100,"Ananta","MECH")
-s2 = Student(101,"Suraj","MECH")
-
-studMng = StudentManager()
-studMng.addStudent(s1)
-studMng.addStudent(s2)
-studMng.deleteStudent(100)
-studMng.printStudentList()
+s1 = Student()
+s2 = Student()
+print(s1.rollNo)
+print(s2.name)
